0f652259feab487dfd8833a52ceed20d09b835d6
Pepijn Hillemans <pepijnhillemans@gmail.com>
2019-06-23 17:26:15 +0200

    made a start on working with components in react

---
+++ client/src/components/firstPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/firstPage.css';
+
+import LobbyScreen from './lobbyScreen'
+
+function Firstpage(props) {
+  return (
+      <div class='container'>
+        {/* hier moet een component met een header */}
+        header --->>> create lobby
+
+        <LobbyScreen/>
+      </div>
+    
+  );
+}
+
+export default Firstpage;
---
+++ client/src/components/lobbyScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/lobbyScreen.css';
+
+function Lobbyscreen(props){
+    return(
+        <div>dit is een lobbyscreen</div>
+    )
+}
+
+export default Lobbyscreen
